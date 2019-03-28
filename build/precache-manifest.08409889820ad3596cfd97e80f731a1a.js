8288bfb53fa19f3ce9064a7e389723a44dcf0b9f
sayed109 <sayed109@sayed109-dell.homerouter.cpe>
2019-03-28 19:24:36 +0300

    publish to GitHub Pages

---
+++ build/precache-manifest.08409889820ad3596cfd97e80f731a1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "79211dc7c19696137f80",
+    "url": "/robofriends/static/js/main.a19da9ee.chunk.js"
+  },
+  {
+    "revision": "2394cf6b139cbcffd241",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "79211dc7c19696137f80",
+    "url": "/robofriends/static/css/main.6db500ad.chunk.css"
+  },
+  {
+    "revision": "2394cf6b139cbcffd241",
+    "url": "/robofriends/static/css/2.fbc00499.chunk.css"
+  },
+  {
+    "revision": "dae776413cd65f230f8a1d971ac36405",
+    "url": "/robofriends/index.html"
+  }
+];
