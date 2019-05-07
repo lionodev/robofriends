143dd9562fdad82d7cc211377b33ea3fcc83b5f4
sayed109 <sayed109@sayed109-dell>
2019-05-07 19:00:01 +0300

    improve accessibility

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,12 +3,12 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
-      <input
+      <label><input
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
         onChange={searchChange}
-      />
+      /></label>
     </div>
   );
 }
